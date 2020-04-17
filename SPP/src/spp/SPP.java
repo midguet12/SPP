8c860a28cb667f8f1b23542aa2178c5e8eb7db97
Seth Díaz <33437473+Sethndd@users.noreply.github.com>
2020-04-17 02:23:24 -0500

    Actualización de estructura de archivos

---
+++ SPP/src/spp/SPP.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp;
+
+import dataaccess.DataBaseConnection;
+
+/**
+ *
+ * @author SNDD_
+ */
+public class SPP {
+    public static void main(String[] args) {
+        DataBaseConnection dbc = new DataBaseConnection();
+    }
+    
+}
