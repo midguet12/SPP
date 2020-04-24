ef0cb3939fa42a528759b7cb8c05003e82ea88bd
Seth Díaz <33437473+Sethndd@users.noreply.github.com>
2020-04-24 05:12:54 -0500

    Added organizationDAO

---
+++ SPP/src/dataaccess/OrganizationDAO.java
@@ -0,0 +1,117 @@
+package dataaccess;
+
+import domain.Organization;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import utilities.ExceptionLogger;
+
+public class OrganizationDAO {
+    private final DataBaseConnection dbc;
+    private Connection connection;
+    private ResultSet resultSet;
+        
+    public OrganizationDAO(){
+        dbc = new DataBaseConnection();
+    }
+    
+    public void insertOrganization(Organization organization){
+        connection = dbc.getConnection();
+        String query = "insert into organization values(?, ?, ?, ?, ?, ?, ?, ?);"; //Consulta
+        
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, organization.getId());
+            preparedStatement.setString(2, organization.getName());
+            preparedStatement.setString(3, organization.getSector());
+            preparedStatement.setString(4, organization.geteMail());
+            preparedStatement.setString(5, organization.getPhoneNumber());
+            preparedStatement.setInt(6, organization.getIdState());
+            preparedStatement.setString(7, organization.getPhoneNumber());
+            preparedStatement.setString(8, organization.getAddress());
+            
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException exception){
+            ExceptionLogger.notify(exception.getMessage());
+        }
+        finally{
+            dbc.closeConnection();
+        }
+    }
+    public Organization getOrganization(String idOrganization){
+        Organization organization = null;
+        connection = dbc.getConnection();
+        String query = "Select * from organization where id_organization = '"+ idOrganization +"';";
+
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);        
+            resultSet = preparedStatement.executeQuery(); 
+            resultSet.next();
+            
+            organization = new Organization(
+                resultSet.getInt("id_organization"), 
+                resultSet.getString("name"),
+                resultSet.getString("sector"),
+                resultSet.getString("email"),
+                resultSet.getString("phone_number"),
+                resultSet.getInt("id_state"),
+                resultSet.getString("City"), 
+                resultSet.getString("Adress"));
+                
+        } 
+        catch (SQLException exception){
+            ExceptionLogger.notify(exception.getMessage());
+        }
+        finally{
+            dbc.closeConnection();
+        }
+        return organization;
+    }
+
+    public void updateOrganization(String idOrganization, Organization organization){
+        connection = dbc.getConnection();
+        String query = "UPDATE organization SET id_organization = ?, name = ?, sector = ?, email = ?, phone_number = ?, id_state = ?, city = ?, adress = ? WHERE id_organization = ?";
+        
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, organization.getId());
+            preparedStatement.setString(2, organization.getName());
+            preparedStatement.setString(3, organization.getSector());
+            preparedStatement.setString(4, organization.geteMail());
+            preparedStatement.setString(5, organization.getPhoneNumber());
+            preparedStatement.setInt(6, organization.getIdState());
+            preparedStatement.setString(7, organization.getCity());
+            preparedStatement.setString(8, organization.getAddress());
+            
+            preparedStatement.setString(9, idOrganization);
+          
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException exception){
+            ExceptionLogger.notify(exception.getMessage());
+        }
+        finally{
+            dbc.closeConnection();
+        }
+    }
+
+    public void deleteOrganization(int idOrganization){
+        connection = dbc.getConnection();
+        String query = "DELETE FROM organization WHERE id_organization = ?";
+        
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, idOrganization);
+            
+            preparedStatement.executeUpdate();
+        }
+        catch(SQLException exception){
+            ExceptionLogger.notify(exception.getMessage());
+        }
+        finally{
+            dbc.closeConnection();
+        }
+    }   
+}
