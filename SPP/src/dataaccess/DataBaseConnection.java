59bda55d5ec511f5045617983f57f67ce8064c1f
Midguet <midguet12@hotmail.com>
2020-04-24 00:53:33 -0500

    Added files

---
+++ SPP/src/dataaccess/DataBaseConnection.java
@@ -0,0 +1,173 @@
+package dataaccess;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import exceptionlog.ExceptionLogger;
+
+//Hasher
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;  
+import java.security.NoSuchAlgorithmException;  
+
+/**
+ *
+ * @author midg
+ */
+public class DataBaseConnection {
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+    
+    private String user;
+    private String password;
+    
+    public DataBaseConnection(){
+        user = "midguet";
+        password = "Magt2208";
+    }
+
+    public Connection startConnection(){ //Metodo para iniciar conexion
+        
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://midguet.ddns.net:3306/spp?useUnicode=yes&characterEncoding=UTF-8", user, password);
+            //Conexion a base de datos, driver + servicio de bd + direccion de servidor + puerto + base de datos + se especifica grupo de caracteres + usuario + contraseña
+            statement = connection.createStatement();
+            //return connection;
+            
+        }
+        catch(SQLException exception){
+            String e = exception.getMessage();
+            System.out.println(e);
+            ExceptionLogger.notify(e);
+        }
+        return connection;
+    }
+    
+    public void closeConnection(){ //Metodo para cerrar conexion 
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } 
+            catch (SQLException exception) {
+                System.out.println(exception.getMessage());
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } 
+                catch (SQLException exception) {
+                    System.out.println(exception.getMessage());
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } 
+                catch (SQLException exception) {
+                    System.out.println(exception.getMessage());
+                }
+            }
+            //System.out.println("Coneccion cerrada");
+    }    
+
+    public void create(){
+
+        startConnection();
+
+        String query = "INSERT INTO customers (name, address) VALUES (?, ?)"; //Consulta
+        
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query); //Se convierte consulta a declaracion
+            preparedStatement.setString(1, "Seth"); //Siendo declaracion, defines el valor del interrogante de acuerdo a su posicion con estos metodos
+            preparedStatement.setString(2, "Coatepec");
+           
+            int rowsInserted = preparedStatement.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("Se agregó correctamente");
+            }
+        }
+        catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally{
+            closeConnection();
+        }
+    }
+
+    public void read(){
+
+        startConnection();
+
+        try{
+            resultSet = statement.executeQuery("Select * from customers");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name")+ "," + resultSet.getString("address"));
+            }
+        } 
+        catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally{
+            closeConnection();
+        }
+    }
+
+    public void update(){
+
+        startConnection();
+
+        String query = "UPDATE customers SET name=? WHERE name=?";
+        
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "Midguet");
+            preparedStatement.setString(2, "Seth");
+           
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Se edito correctamente");
+            }
+        }
+        catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally{
+            closeConnection();
+        }
+    }
+
+    public void delete(){
+
+        startConnection();
+
+        String query = "DELETE FROM customers WHERE name=?";
+        
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,"Seth");
+            
+            int rowsDeleted = preparedStatement.executeUpdate();
+            if(rowsDeleted>0){
+                System.out.println("Se elimino correctamente");
+            }
+        }
+        catch(SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally{
+            closeConnection();
+        }
+    }
+    
+
+    
+    
+}
---
+++ SPP/src/spp/SPP.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp;
+
+import dataaccess.Coordinator;
+import dataaccess.DataBaseConnection;
+import dataaccess.Encrypter;
+import exceptionlog.ExceptionLogger;
+
+/**
+ *
+ * @author SNDD_
+ */
+public class SPP {
+    public static void main(String[] args) {
+        //Coordinator coordinator1 = new Coordinator(18012193,"Magt2208","Midguet","Garcia");
+        Coordinator coordinator2 = new Coordinator(18012193,"Magt2208");
+        
+        String x = coordinator2.getName();
+        System.out.println(x);
+        
+        
+        
+        
+        
+        
+        
+        
+
+    }
+    
+}
