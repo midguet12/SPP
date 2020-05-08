6bc4be924ff19f41f09360f846040d7f74dd6efb
Midguet <midguet12@hotmail.com>
2020-05-08 16:59:58 -0500

    Add FTP class

--- SPP/src/spp/SPP.java
+++ SPP/src/spp/SPP.java
@@ -1,90 +1,20 @@
 package spp;
 
-import bussineslogic.StringChecker;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+
 import java.io.OutputStream;
-//import org.apache.commons.net.ftp.FTP;
+
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
-import sun.net.ftp.FtpClient;
+
 
 public class SPP {
     public static void main(String[] args) {
        
-        File file = new File("hola.pdf");
-        
-        
-        
-        
-        
-        
-        
-        FTPClient ftp = new FTPClient();
-        
-        try{
-            String ftpServer = "midguet.ddns.net";
-            String user = "spp";
-            String password = "Magt2208";
-            
-            ftp.connect(ftpServer);
-            
-            
-            
-            System.out.println(ftp.login(user, password));
-            
-            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
-            ftp.changeWorkingDirectory("/mnt/user/spp");
-            
-            
-            
-            
-            /*FTPFile[] ftpl = ftp.listFiles();
-            
-            for (FTPFile ff : ftpl) {
-                System.out.println(ff.getName());
-            }*/
-            
-            
-            OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
-            
-            
-            System.out.println(ftp.retrieveFile(file.getName(), out));
-            out.close();
-            //Cargar archivo
-            //System.out.println(ftp.storeFile("error.log", new FileInputStream(file)));
-            
-            
-            
-            
-            
-        } catch(Exception ex){
-            System.out.println(ex.getMessage());
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    }
+  
 
-   
+    }
 }
---
+++ SPP/src/utilities/FileClient.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.net.ftp.FTPClient;
+
+/**
+ *
+ * @author midgu
+ */
+public class FileClient {
+    String serverPath = "/mnt/user/spp";
+    String server = "midguet.ddns.net";
+    String password = "Magt2208";
+
+    
+    String path = "/mnt/user/spp";
+    File file = null;
+    
+    FTPClient ftp = new FTPClient();
+    
+    
+        
+
+    public FileClient() {
+        try {
+            ftp.connect(server);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FileClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public boolean insertFile(){
+        
+        boolean b = false;
+        
+        try {
+            
+            ftp.login(server, password);
+            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+            ftp.changeWorkingDirectory(path);
+            
+            b = ftp.storeFile("hola.pdf",  new FileInputStream(file));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FileClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return b;
+        
+    }
+    
+    public boolean getFile(){
+        boolean b = false;
+        
+        OutputStream out;
+        try {
+            
+            ftp.login(server, password);
+            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+            ftp.changeWorkingDirectory(path);
+            
+            out = new BufferedOutputStream(new FileOutputStream(file));
+            b = ftp.retrieveFile(file.getName(), out);
+            out.close();
+            
+        } catch (Exception ex) {
+            Logger.getLogger(FileClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        return b;
+        
+    }
+    
+    
+}
