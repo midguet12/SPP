0f8779da3d09de3fcae0958c3e70fa84fe6f3dc0
Midguet <midguet12@hotmail.com>
2020-05-08 15:59:01 -0500

    FTP upload

--- SPP/src/spp/SPP.java
+++ SPP/src/spp/SPP.java
@@ -1,10 +1,83 @@
 package spp;
 
 import bussineslogic.StringChecker;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+//import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import sun.net.ftp.FtpClient;
 
 public class SPP {
     public static void main(String[] args) {
-        String string = "S1812147";
-        System.out.println(StringChecker.isIDIntern(string));
+       
+        File file = new File("error.log");
+        //OutputStream out = new OutputStream();
+        
+        
+        
+        FTPClient ftp = new FTPClient();
+        
+        try{
+            String ftpServer = "midguet.ddns.net";
+            String user = "spp";
+            String password = "Magt2208";
+            
+            ftp.connect(ftpServer);
+            
+            
+            
+            System.out.println(ftp.login(user, password));
+            
+            ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+            ftp.changeWorkingDirectory("/mnt/user/spp");
+            
+            
+            
+            
+            /*FTPFile[] ftpl = ftp.listFiles();
+            
+            for (FTPFile ff : ftpl) {
+                System.out.println(ff.getName());
+            }*/
+            
+            
+            
+            
+            //ftp.retrieveFile("hola.pdf", local);
+            
+            System.out.println(ftp.storeFile("error.log", new FileInputStream(file)));
+            
+            
+            
+            
+            
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+
+   
 }
