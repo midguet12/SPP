33fc2c7a07ae051540e244be2e6a8a29f8bd51b1
Midguet <midguet12@hotmail.com>
2020-06-05 15:46:18 -0500

    Update SPP.java

--- SPP/src/spp/SPP.java
+++ SPP/src/spp/SPP.java
@@ -1,11 +1,21 @@
 package spp;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 
 
 
 
-public class SPP{   
+
+public class SPP extends Application{   
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    
+        //El codigo de la clase que quieres ejecutar va aqui
+    }
+    
     
     
         
